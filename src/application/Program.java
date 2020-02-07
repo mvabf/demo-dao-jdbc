82c0c5eb34b22811d0094ab0bf08df98ed849c88
Marcus Venicius <mvabf_dev@outlook.com>
2020-02-06 23:27:38 -0300

    SellerDao e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,8 +16,9 @@ public class Program {
 		Department dp = new Department(1, "books");
 		System.out.println(dp);
 		
-		
 		Seller sl = new Seller(21, "Marcus", "marcus@gmail.com",sdf.parse("10/05/1994"), 13000.0,dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		System.out.println(sl);
 
 	}
